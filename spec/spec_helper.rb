baff65c97981628f6955e641f08f850cd85df2c0
Patrick Dayton <daytonpa@gmail.com>
2020-01-20 15:13:41 -0500

    Fix unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,19 @@
 require 'chefspec'
 require 'chefspec/policyfile'
+
+module PlatformVersions
+  @centos = %w( 8 )
+  @ubuntu = %w( 18.04 )
+
+  def self.amazon
+    @amazon
+  end
+
+  def self.centos
+    @centos
+  end
+
+  def self.ubuntu
+    @ubuntu
+  end
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,23 +7,23 @@
 require 'spec_helper'
 
 describe 'docker-prometheus::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+  {
+    'centos' => PlatformVersions.centos,
+    'ubuntu' => PlatformVersions.ubuntu,
+  }.each do |platform_name, platform_versions|
+    platform_versions.each do |platform_version|
+      context "When all attributes are default, on #{platform_name} #{platform_version}" do
+        before do
+          stub_command("/opt/prometheus/prometheus --version 2>&1 | grep '2.15.2'").and_return(false)
+        end
+        # for a complete list of available platforms and versions see:
+        # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+        platform platform_name, platform_version
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+        it 'converges successfully' do
+          expect { chef_run }.to_not raise_error
+        end
+      end
     end
   end
 end
